95455489dbfc61139901f2d7ff83d2b48188b7fa
Leandro Lima <lecoreislima_z2@hotmail.com>
2026-04-14 13:34:33 -0300

    Adicionando tela de login ao projeto

---
+++ OneDrive/Documentos/NetBeansProjects/controle-matriculas-educontrol/src/main/java/com/mycompany/controle/matriculas/educontrol/LoginBean.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.controle.matriculas.educontrol;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+/**
+ *
+ * @author leo
+ */
+
+@ManagedBean (name = "loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    // Ações da tela de login
+    
+    public String login() {
+        if("admin".equals(usuario) && "admin".equals(senha)) {
+            return "index.xhtml?faces-redirect=true"; // redireciona para o menu principal
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Usuário ou Senha Inválidos", null);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            return null; // permanece na mesma página
+        }
+    }
+
+    public String cadastrar() {
+        return "cadastro.xhtml?faces-redirect=true";
+    }
+
+    public String sair() {
+        return "login.xhtml?faces-redirect=true";
+    }
+}
